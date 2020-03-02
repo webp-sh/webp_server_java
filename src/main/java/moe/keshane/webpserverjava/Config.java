2fd55b83f3ac2ac52459054029cf317d0b29fab4
keshanechen <keshanechen@gmail.com>
2020-03-02 14:25:31 +0800

    add request uri to image path

--- src/main/java/moe/keshane/webpserverjava/Config.java
+++ src/main/java/moe/keshane/webpserverjava/Config.java
@@ -1,5 +1,53 @@
 package moe.keshane.webpserverjava;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+
 public class Config {
+    public String host;
+    public int port;
+    public Map<String,String> imgMap;
+    public List<String> allowedTypes;
+
+    public Config(){}
+
+    @Override
+    public String toString() {
+        return "Config{" +
+                "host='" + host + '\'' +
+                ", port='" + port + '\'' +
+                ", imgMap=" + imgMap +
+                ", allowedTypes=" + allowedTypes +
+                '}';
+    }
+
+    public static Config readConfig(String path) throws IOException {
+        ObjectMapper json = new ObjectMapper();
+        Config config = json.readValue(new File(path), Config.class);
+        return config;
+    }
+
+    public String matchImgMap(String requestUri){
+        return imgMap.get(requestUri);
+    }
+
+    public String getRealImagePath(String requestUri){
+        String[] splitUri = requestUri.split("/");
+        String imageName = splitUri[splitUri.length-1];
+        int to = splitUri.length>2?splitUri.length - 1:2;
+        String[] dPath = Arrays.copyOfRange(splitUri, 0, to);
+        String requestPath = String.join("/",dPath);
+        return imgMap.get(requestPath)+"/"+imageName;
+    }
+
+    public String getPort(){
+        return port!=0?String.valueOf(port):"8080";
+    }
 
 }
--- src/main/java/moe/keshane/webpserverjava/WebpServerJavaApplication.java
+++ src/main/java/moe/keshane/webpserverjava/WebpServerJavaApplication.java
@@ -1,6 +1,7 @@
 package moe.keshane.webpserverjava;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -10,22 +11,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Controller
 @SpringBootApplication
 public class WebpServerJavaApplication {
     private static Config config;
+    private static Logger log = LoggerFactory.getLogger(WebpServerJavaApplication.class);
+
 
     public static void main(String[] args) {
         String configPath = args[0];
-        System.out.println(args.toString());
+        log.debug(args.toString());
         try {
             config = Config.readConfig(configPath);
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException("config file error");
         }
-        System.out.println(config.toString());
+        log.debug(config.toString());
         SpringApplication app = new SpringApplication(WebpServerJavaApplication.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", config.getPort()));
         app.run(args);
@@ -36,8 +41,15 @@ public class WebpServerJavaApplication {
     @RequestMapping("/**")
     public String output(HttpServletRequest request){
         String uri = request.getRequestURI();
-        return uri;
+//        String[] sarr = Arrays.stream(uri.split("/"))
+//                .filter(value -> value != null && value.length() > 0 && value!="")
+//                .toArray(size -> new String[size]);
+//        String s = String.join("/", sarr);
+        String realImagePath = config.getRealImagePath(uri);
+        return realImagePath;
     }
 
-    
+    public boolean isImageExist(String imagePath){
+        return new File(imagePath).exists();
+    }
 }
